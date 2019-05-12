0def1617a82db1d7319c77cf0a94ccbd5044b53d
chrisfoose <47850167+chrisfoose@users.noreply.github.com>
2019-05-12 05:46:37 -1000

    Create MovieDiscount.cs

---
+++ MovieDiscount.cs
@@ -0,0 +1,19 @@
+//Use this program to calculate a movie discount
+//Christopher Foose
+using System;
+class MovieDiscount
+{
+   static void Main()
+   {
+      int age = 10;
+      char rating = 'R';
+      const int CHILD_AGE = 12;
+      const int SENIOR_AGE = 65;
+      Console.WriteLine("When age is {0} and rating is {1}",
+         age, rating);
+      if((age <= CHILD_AGE || age >= SENIOR_AGE) && rating == 'G')
+         Console.WriteLine("Discount applies");
+      else
+         Console.WriteLine("Full price");
+   }
+}
